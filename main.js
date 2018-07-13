e54a138c2ede1699f18e0b31a14efd2c5a4c5ad5
Kevin Bandy <kevin@bandydesign.com>
2018-07-13 19:58:14 -0400

    Add a competency bar to each element in the primary color for the technology

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ function init() {
 
 function buildTechnologyBlock(technology) {
 	var $outerDiv = $('<li>');
+	var $progressBar = $('<div>');
+	var $progressBarValue = $('<div>');
+	var $innerDiv = $('<div>');
 	var $image = $('<img>');
 	var $contentArea = $('<div>');
 	var $heading = $('<h5>');
@@ -18,6 +21,15 @@ function buildTechnologyBlock(technology) {
 	var $description = $('<p>');
 
 	$outerDiv.addClass('technology technology-' + generateSlug(technology.technology));
+	$innerDiv.addClass('technology-content');
+	
+	$progressBar.addClass('technology-competence-bar')
+	$progressBarValue
+		.addClass('technology-competence-bar-value')
+		.css({
+			width: technology.competence + '%',
+			background: technology.primaryColor
+		});
 
 	$image
 		.addClass('technology-logo')
@@ -32,11 +44,14 @@ function buildTechnologyBlock(technology) {
 		.text(technology.level)
 		.addClass('technology-level technology-level-' + generateSlug(technology.level));
 
-	$outerDiv.append($image);
+	$progressBar.append($progressBarValue)
+	$outerDiv.append($progressBar);
+	$innerDiv.append($image);
 	$heading.append($tag);
 	$contentArea.append($heading);
 	$contentArea.append($description);
-	$outerDiv.append($contentArea);
+	$innerDiv.append($contentArea);
+	$outerDiv.append($innerDiv);
 
 	return $outerDiv;
 }
