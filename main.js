399450b32fc796d8d9f03836ed0f6f137ddf8912
kbandy <kbandy@shootproofs-MacBook-Pro-2.local>
2018-07-13 15:56:49 -0400

    Create initial page with technology list * Technologies are populated from a list in technologies.js * jQuery is used to loop through and build the list structure * Some basic styles are added to the list portion of the site

---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).ready(init);
+
+function init() {
+	console.log('Welcome to Kevin Bandy\'s web development portfolio. I just want to touch on a few technologies I have experience with and give you an overview of my skills.');
+	console.log(technologies);
+
+	technologies.forEach(function assembleTechnologiesList(technology) {
+		$('#technologies-list').append(buildTechnologyBlock(technology));
+	});
+}
+
+function buildTechnologyBlock(technology) {
+	var $outerDiv = $('<li>');
+	var $image = $('<img>');
+	var $contentArea = $('<div>');
+	var $heading = $('<h5>');
+	var $tag = $('<strong>');
+	var $description = $('<p>');
+
+	$outerDiv.addClass('technology technology-' + generateSlug(technology.technology));
+
+	$image
+		.addClass('technology-logo')
+		.attr("href", getLogoUri(technology.logo));
+	$heading
+		.addClass('technology-heading')
+		.text(technology.technology);
+	$description
+		.addClass('technology-description')
+		.text(technology.description);
+	$tag
+		.text(technology.level)
+		.addClass('technology-level technology-level-' + generateSlug(technology.level));
+
+	$outerDiv.append($image);
+	$heading.append($tag);
+	$contentArea.append($heading);
+	$contentArea.append($description);
+	$outerDiv.append($contentArea);
+
+	return $outerDiv;
+}
+
+function generateSlug(inputString) {
+	var spaceReplacedString = inputString.replace(/\/|\s|-|_/g, ' ');
+	var spaceReducedString = spaceReplacedString.replace(/\s+/g, ' ');
+
+	return spaceReducedString.split(' ').join('-').toLowerCase();
+}
+
+function getLogoUri(logoSlug) {
+	return "images/logo_" + logoSlug + ".svg";
+}
---
+++ technologies.js
@@ -0,0 +1,56 @@
+var technologies = [
+	{
+		technology: "Angular",
+		level: "Expert",
+		description: "Application architecture, building, maintaining",
+		logo: "angular"
+	},
+	{
+		technology: "SVG",
+		level: "Expert",
+		description: "Digging deep into SVG and its applications.",
+		logo: "svg"
+	},
+	{
+		technology: "HTML / CSS",
+		level: "Expert",
+		description: "Up to date with the latest in HTML5 and CSS3 standards",
+		logo: "html5"
+	},
+	{
+		technology: "SaSS",
+		level: "Expert",
+		description: "Deep knowledge of CSS pre-processors for efficient, maintainable code",
+		logo: "sass"
+	},
+	{
+		technology: "Javascript",
+		level: "Expert",
+		description: "Deep understanding of the language and how to write efficient, scalable code",
+		logo: "js"
+	},
+	{
+		technology: "MySQL",
+		level: "Intermediate",
+		description: "Strengths in architecting db schema and data retrieval",
+		logo: "mysql"
+	},
+	{
+		technology: "PHP",
+		level: "Intermediate",
+		description: "Object oriented programming and architecture, experienced in 1.x Zend Framework",
+		logo: "php"
+	},
+	{
+		technology: "React Native",
+		level: "Basic",
+		description: "Written a few non-production apps, nothing long term",
+		logo: "react_native"
+	},
+	{
+		technology: "Flutter",
+		level: "Basic",
+		description: "Just starting to dig in and learn this exciting offering from Google to build single codebase mobile apps!",
+		logo: "flutter"
+	}
+];
