519d01f9ff5d89f8c253edf3e93ae451efdc9ecd
Kevin Bandy <kevin@bandydesign.com>
2018-07-14 00:09:40 -0400

    Apply to all parallax items

--- main.js
+++ main.js
@@ -27,10 +27,13 @@ function init() {
 
 	function deviceMotionHandler(e) {
 		var inputX = Math.floor(e.accelerationIncludingGravity.x * 10);
-		var inputY = Math.floor(e.accelerationIncludingGravity.y * 10 + 30);
+		var inputY = Math.floor(e.accelerationIncludingGravity.y * 10 + 70);
 		//var motion = smootheMotion(inputX, inputY);
 		$('#output').html('<pre>v2 movement: ' + inputX + ', ' + inputY + '</pre>');// + 'translates to: x:' + motion.x + ', ' + motion.y + '</pre><hr><pre>[' + motionBuffer + ']</pre>');
 		$('.parallax-browser').css('transform', 'translate(' + inputX + 'px, ' + (inputY * -1) + 'px)');
+		$('.parallax-browser-content').css('transform', 'translate(' + (inputX * 2) + 'px, ' + (inputY * -2) + 'px)');
+		$('.parallax-scatter-small').css('transform', 'translate(' + (inputX * .5) + 'px, ' + (inputY * -.5) + 'px)');
+		$('.parallax-scatter-large').css('transform', 'translate(' + (inputX * 4) + 'px, ' + (inputY * -4) + 'px)');
 	}
 
 	var maxBufferSize = 3;
